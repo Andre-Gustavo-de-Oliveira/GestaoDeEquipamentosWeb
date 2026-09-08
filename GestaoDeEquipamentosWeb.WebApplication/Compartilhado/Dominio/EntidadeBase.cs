27b6cbeb7eceb8bc7499c03d2d86adc821982e43
André Oliveira <andre.oliveira.contato@outlook.com>
2026-09-07 22:57:29 -0300

    Adiconado ENTIDADEBASE dentro da pasta DOMINIO nos arquivos COMPARTILHADOS

---
+++ GestaoDeEquipamentosWeb.WebApplication/Compartilhado/Dominio/EntidadeBase.cs
@@ -0,0 +1,8 @@
+namespace GestaoDeEquipamentosWeb.WebApp.Compartilhado.Dominio;
+
+public abstract class EntidadeBase
+{
+    public int Id { get; set; }
+
+    public abstract void Atualizar(EntidadeBase entidadeAtualizada);
+}
