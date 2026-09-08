838d95bd7698cc2ad1f6e48e278036125ff2dfed
André Oliveira <andre.oliveira.contato@outlook.com>
2026-09-07 22:50:01 -0300

    Adicionado a imagem de fundo em WWWROOT/IMG Adicionado Código compartilhado na PROGRAM Configurado os arquivos principais dos arquivos JSON

--- GestaoDeEquipamentosWeb.WebApplication/Program.cs
+++ GestaoDeEquipamentosWeb.WebApplication/Program.cs
@@ -1,26 +1,17 @@
+using GestaoDeEquipamentosWeb.WebApp.Compartilhado.Apresesntacao;
+using GestaoDeEquipamentosWeb.WebApp.Compartilhado.Infraestrutura;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-builder.Services.AddRazorPages();
+builder.Services.AdicionarCamadaDeInfraestrutura();
+
+builder.Services.AdicionarCamadaDeApresentacao();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (!app.Environment.IsDevelopment())
-{
-    app.UseExceptionHandler("/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-    app.UseHsts();
-}
-
-app.UseHttpsRedirection();
-
 app.UseRouting();
+app.MapDefaultControllerRoute();
 
-app.UseAuthorization();
-
-app.MapStaticAssets();
-app.MapRazorPages()
-   .WithStaticAssets();
+app.UseStaticFiles();
 
 app.Run();
