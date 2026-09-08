3f0b381abdd8868d6e9fa93799c6b5c68d1bf5d9
André Oliveira <andre.oliveira.contato@outlook.com>
2026-09-07 22:56:05 -0300

    Adicionado os arquivos e alterações dentro da pasta APRESENTACAO

---
+++ GestaoDeEquipamentosWeb.WebApplication/Compartilhado/Apresentacao/InjecaoDeDependencia.cs
@@ -0,0 +1,17 @@
+namespace GestaoDeEquipamentosWeb.WebApp.Compartilhado.Apresesntacao;
+
+public static class InjecaoDeDependencia
+{
+    public static void AdicionarCamadaDeApresentacao(this IServiceCollection services)
+    {
+
+        services.AddControllersWithViews().AddRazorOptions(options =>
+        {
+            options.ViewLocationFormats.Clear();
+
+            options.ViewLocationFormats.Add("/Compartilhado/Apresentacao/Views/{0}.cshtml");
+
+            options.ViewLocationFormats.Add("/Modulos/{1}s/Apresentacao/Views/{0}.cshtml");
+        });
+    }
+}
